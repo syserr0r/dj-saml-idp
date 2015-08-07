452287cdec1c4d56f3c0d9f8d0669d0bf7d2fd94
Sebastian Vetter <seb@roadsi.de>
2015-08-07 15:21:17 -0700

    Add release tooling and documentation

--- setup.py
+++ setup.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals, absolute_import
 from setuptools import setup
 
 
-with open('README.md') as readme:
+with open('README.rst') as readme:
     description = readme.read()
 
 
 setup(
-    name = 'dj-saml-idp',
-    version = '0.19.0',
-    author = 'Sebastian Vetter',
-    author_email = 'sebastian@mobify.com',
-    description = 'SAML 2.0 IdP for Django',
-    long_description = description,
-    install_requires = [
+    name='dj-saml-idp',
+    version='0.19.0',
+    author='Sebastian Vetter',
+    author_email='sebastian@mobify.com',
+    description='SAML 2.0 IdP for Django',
+    long_description=description,
+    install_requires=[
         'Django>=1.4',
         'M2Crypto>=0.20.1',
         'BeautifulSoup>=3.2.0'],
-    license = 'MIT',
-    packages = ['saml2idp',
-                'saml2idp.tests'],
-    package_dir = {'saml2idp': 'saml2idp'},
-    url = 'http://github.com/mobify/dj-saml-idp',
-    zip_safe = True,
+    license='MIT',
+    packages=['saml2idp',
+              'saml2idp.tests'],
+    url='http://github.com/mobify/dj-saml-idp',
+    zip_safe=False,
 )
