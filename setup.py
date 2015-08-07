7871b8fc3819cdf628ff874665f6ae3614632a84
Sebastian Vetter <seb@roadsi.de>
2015-08-07 09:20:35 -0700

    Rename project repo and package

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
 from setuptools import setup
 
+
+with open('README.md') as readme:
+    description = readme.read()
+
+
 setup(
-    name = 'saml2idp',
-    version = '0.18.1',
-    author = 'John Samuel Anderson',
-    author_email = 'john@andersoninnovative.com',
+    name = 'dj-saml-idp',
+    version = '0.19.0',
+    author = 'Sebastian Vetter',
+    author_email = 'sebastian@mobify.com',
     description = 'SAML 2.0 IdP for Django',
-    long_description = 'SAML 2.0 Identity Provider app for Django projects.',
+    long_description = description,
     install_requires = [
         'Django>=1.4',
         'M2Crypto>=0.20.1',
-        'BeautifulSoup>=3.2.0',
-    ],
+        'BeautifulSoup>=3.2.0'],
     license = 'MIT',
     packages = ['saml2idp', 'saml2idp.tests'],
     package_dir = {'saml2idp': 'idptest/saml2idp'},
-    package_data = {'saml2idp': ['templates/saml2idp/*.html', 'templates/saml2idp/*.xml']},
-    url = 'http://code.google.com/p/django-saml2-idp/',
+    package_data = {'saml2idp': ['templates/saml2idp/*.html',
+                                 'templates/saml2idp/*.xml']},
+    url = 'http://github.com/mobify/dj-saml-idp',
     zip_safe = True,
 )
